b05a470a5d7dd793a10435d17aba852957b23c7a
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2022-08-16 17:40:17 -0700

    added url hash extension

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
-const splide = new Splide('.splide', {
-  type: 'slide', //slide, loop, fade
+// const splide = new Splide('.splide', {
+//   type: 'slide', //slide, loop, fade
+//   // padding: { y: 10 }, //css values
+//   gap: '20px', //1 rem default
+//   // rewind: true,
+//   speed: 500, //transition in milliseconds
+//   // fixedWidth: '10vw',
+//   // width: '100vw',
+//   perPage: 5,
+//   start: 3,
+//   perMove: 2,
+//   autoplay: true,
+//   interval: 3000,
+//   arrows: false,
+//   pagination: false,
+//   pauseOnHover: true,
+//   wheel: true,
+// });
+// splide.mount();
+
+
+
+var main = new Splide('.splide', {
+  type: 'fade', //slide, loop, fade
   // padding: { y: 10 }, //css values
-  gap: '20px', //1 rem default
-  // rewind: true,
-  speed: 500, //transition in milliseconds
+  // gap: '20px', //1 rem default
+  rewind: true,
+  // speed: 500, //transition in milliseconds
   // fixedWidth: '10vw',
   // width: '100vw',
-  perPage: 5,
-  start: 3,
-  perMove: 2,
-  autoplay: true,
-  interval: 3000,
-  arrows: false,
+  // perPage: 5,
+  // start: 3,
+  // perMove: 2,
+  // autoplay: true,
+  // interval: 3000,
+  arrows: true,
   pagination: false,
-  pauseOnHover: true,
-  wheel: true,
+  // pauseOnHover: true,
+  // wheel: true,
 });
-splide.mount();
+
+counter = 1
+main.mount();
